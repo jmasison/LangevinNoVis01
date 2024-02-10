7276d668a22cfe7565ca3c2f71063cea18c6f204
Jim Schaff <schaff.jim@gmail.com>
2024-02-10 09:59:02 -0500

    comment-out MockWebServer testing of sending remote messages.

--- src/test/java/org/vcell/messaging/VCellMessagingRestTest.java
+++ src/test/java/org/vcell/messaging/VCellMessagingRestTest.java
@@ -1,9 +1,9 @@
 package org.vcell.messaging;
 
-import okhttp3.HttpUrl;
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
-import okhttp3.mockwebserver.RecordedRequest;
+//import okhttp3.HttpUrl;
+//import okhttp3.mockwebserver.MockResponse;
+//import okhttp3.mockwebserver.MockWebServer;
+//import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,64 +15,64 @@ import java.util.Properties;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VCellMessagingRestTest {
-    private MockWebServer mockWebServer;
+//    private MockWebServer mockWebServer;
 
-    @BeforeEach
-    public void setUp() throws IOException {
-        mockWebServer = new MockWebServer();
-        mockWebServer.start();
-    }
+//    @BeforeEach
+//    public void setUp() throws IOException {
+//        mockWebServer = new MockWebServer();
+//        mockWebServer.start();
+//    }
 
-    @AfterEach
-    public void tearDown() throws IOException {
-        mockWebServer.shutdown();
-    }
+//    @AfterEach
+//    public void tearDown() throws IOException {
+//        mockWebServer.shutdown();
+//    }
 
-    @Test
-    public void testSendWorkerEvent_starting() throws Exception {
-        // Arrange
-        mockWebServer.enqueue(new MockResponse().setBody("OK"));
-
-        HttpUrl url = mockWebServer.url("/");
-
-        MessagingConfig config = new MessagingConfig (
-                url.host(),
-                url.port(),
-                "msg_user",
-                "msg_pswd",
-                "localhost",
-                "schaff",
-                "12334483837",
-                0,
-                0
-                );
-
-        VCellMessagingRest vCellMessagingRest = new VCellMessagingRest(config);
-
-        String expectedPath =
-                "/api/message/workerEvent" +
-                        "?type=queue" +
-                        "&JMSPriority=5" +
-                        "&JMSTimeToLive=600000" +
-                        "&JMSDeliveryMode=persistent" +
-                        "&MessageType=WorkerEvent" +
-                        "&UserName=schaff" +
-                        "&HostName=localhost" +
-                        "&SimKey=12334483837" +
-                        "&TaskID=0" +
-                        "&JobIndex=0" +
-                        "&WorkerEvent_Status=JOB_STARTING" +
-                        "&WorkerEvent_StatusMsg=Starting+Job" +
-                        "&WorkerEvent_Progress=0.0" +
-                        "&WorkerEvent_TimePoint=0.0";
-
-        vCellMessagingRest.sendWorkerEvent(WorkerEvent.startingEvent("Starting Job"));
-
-        RecordedRequest request = mockWebServer.takeRequest();
-        assertEquals("POST", request.getMethod());
-        assertEquals(expectedPath, request.getPath());
-        assertEquals("", request.getBody().readUtf8());
-    }
+//    @Test
+//    public void testSendWorkerEvent_starting() throws Exception {
+//        // Arrange
+//        mockWebServer.enqueue(new MockResponse().setBody("OK"));
+//
+//        HttpUrl url = mockWebServer.url("/");
+//
+//        MessagingConfig config = new MessagingConfig (
+//                url.host(),
+//                url.port(),
+//                "msg_user",
+//                "msg_pswd",
+//                "localhost",
+//                "schaff",
+//                "12334483837",
+//                0,
+//                0
+//                );
+//
+//        VCellMessagingRest vCellMessagingRest = new VCellMessagingRest(config);
+//
+//        String expectedPath =
+//                "/api/message/workerEvent" +
+//                        "?type=queue" +
+//                        "&JMSPriority=5" +
+//                        "&JMSTimeToLive=600000" +
+//                        "&JMSDeliveryMode=persistent" +
+//                        "&MessageType=WorkerEvent" +
+//                        "&UserName=schaff" +
+//                        "&HostName=localhost" +
+//                        "&SimKey=12334483837" +
+//                        "&TaskID=0" +
+//                        "&JobIndex=0" +
+//                        "&WorkerEvent_Status=JOB_STARTING" +
+//                        "&WorkerEvent_StatusMsg=Starting+Job" +
+//                        "&WorkerEvent_Progress=0.0" +
+//                        "&WorkerEvent_TimePoint=0.0";
+//
+//        vCellMessagingRest.sendWorkerEvent(WorkerEvent.startingEvent("Starting Job"));
+//
+//        RecordedRequest request = mockWebServer.takeRequest();
+//        assertEquals("POST", request.getMethod());
+//        assertEquals(expectedPath, request.getPath());
+//        assertEquals("", request.getBody().readUtf8());
+//    }
 
     @Test
     public void testMessagingConfig() throws IOException {
